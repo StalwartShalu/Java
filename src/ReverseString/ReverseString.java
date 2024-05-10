1da16181873dbdb9e51cee4ca09aeb053fc08c8a
StalwartShalu <shaliniselvam098@gmail.com>
2024-05-10 00:42:17 -0700

    initial Push

---
+++ src/ReverseString/ReverseString.java
@@ -0,0 +1,21 @@
+package ReverseString;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("----------Reverse a String----------");
+		System.out.println("Original String: ");
+		String input = sc.nextLine();
+		StringBuilder str = new StringBuilder();
+		str.append(input);
+		System.out.println("Reverse String: " +str.reverse());	
+	
+		
+	}
+
+}
