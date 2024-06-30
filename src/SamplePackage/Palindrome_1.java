05a6942acd6cd60c417f80fd222da439ad290985
StalwartShalu <shaliniselvam098@gmail.com>
2024-06-30 07:40:27 -0700

    New Push

--- src/SamplePackage/Palindrome_1.java
+++ src/SamplePackage/Palindrome_1.java
@@ -1,4 +1,4 @@
-package src.PalindromCheck;
+package SamplePackage;
 
 import java.util.Scanner;
 
--- src/SamplePackage/ReverseStringLoop.java
+++ src/SamplePackage/ReverseStringLoop.java
@@ -1,21 +1,23 @@
-package src.ReverseString;
+package SamplePackage;
 
 import java.util.Scanner;
 
-public class ReverseString {
+public class ReverseStringLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("----------Reverse a String----------");
 		System.out.println("Original String: ");
-		String input = sc.nextLine();
-		StringBuilder str = new StringBuilder();
-		str.append(input);
-		System.out.println("Reverse String: " +str.reverse());	
-	
-		
+		String str = sc.nextLine();
+		String nstr = "";
+		for(int i=0; i<str.length(); i++) {
+			char output= str.charAt(i);
+			nstr= output+nstr;
+		}
+		System.out.print("Reversed String: " +nstr);
+
 	}
 
 }
